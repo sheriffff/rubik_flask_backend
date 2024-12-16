a81d3dd0868aa064bf8c33bc8f5f28edce87c89f
sheriff <manuel.lopez.sheriff@gmail.com>
2024-12-16 10:57:22 +0100

    Run with ssl cert

--- api.py
+++ api.py
@@ -209,4 +209,4 @@ def delete_note(note_id):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, ssl_context=("cert.pem", "key.pem"))
